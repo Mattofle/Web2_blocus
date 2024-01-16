a5856e9bff761a6ec0269362568f4d9b64dff710
Erismann <matteo.erismann@student.vinci.be>
2024-01-16 19:28:19 +0100

    2.9: dynamic page content

---
+++ B_Exercices/2.9/js-basic-boilerplate-main/src/index.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+import lasVegasUrl from './img/5028486419944_1.jpg';
+import mishimaUrl from './img/SD2zJgkpw48fYDV15DU2gFI98ZyTgj_large.jpg';
+
+const main = document.querySelector('main');
+
+renderhomepage();
+
+function renderhomepage() {
+  const homepageHtml = `
+  <div class="row">
+          <div id="block_1" class="text-center col-lg-6 col-xs-12">
+            <img src="${lasVegasUrl}" class="img-thumbnail w-50">
+            <p>T'as johnny Dep il se barre a Las vegas avec un kho a lui et ils se pêtent la geule de ouf 
+              mon reuf. Wlh il sont chépère, charger comme des éléfants</p>
+          </div>
+  
+          <div id="block_2" class="text-center col-lg-6 col-xs-12">
+            <img src="${mishimaUrl}" class="img-thumbnail w-50">
+            <p>Aloes la mon reuf on est sur du conceptuel de ouf, tu suis un vrai écrivain jap dans sa 
+              vie et et ses histoire et wlh c space mais t'as vu c bo</p>
+          </div>
+  </div>
+  <button id="about" type="button">Click ici mon khey!</button>
+  `
+  
+  main.innerHTML = homepageHtml;
+  const button = document.querySelector('button');
+  button.addEventListener('click', renderinfo)
+}
+
+
+function renderinfo() {
+  const info = `
+  <p> la page a été fait par un ptit hérisson </p>
+  <button id="about" type="button">Click ici mon khey!</button>
+  `
+  main.innerHTML = info
+  const button = document.querySelector('button');
+  button.addEventListener('click', renderhomepage)
+}
